3cb82a6024df91f569d2b83c13f8a0ddce0d3a2d
andriimer <shapovalandrii34@gmail.com>
2024-12-26 13:23:14 +0200

    added files

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div>© Company. All right resirved.</div>
+      <div></div>
+    </>
+  )
+}
+
+export default Footer
